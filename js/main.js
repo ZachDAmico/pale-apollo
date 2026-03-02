8427fa0a39d801820cf1f601e58745c323f093c8
Zach D'Amico <zachdamico90@gmail.com>
2026-03-02 16:54:42 -0600

    play/pause button behavior now working fully

--- js/main.js
+++ js/main.js
@@ -38,8 +38,6 @@ const playTracks = () => {
 
   if (playTrackOrder.paused) {
     playTrackOrder.play();
-  } else {
-    playTrackOrder.pause();
     playPauseSwitch.innerHTML = `
 <svg xmlns="http://www.w3.org/2000/svg"
      fill="none"
@@ -52,7 +50,23 @@ const playTracks = () => {
         d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
 </svg>
 `;
+  } else {
+    playTrackOrder.pause();
+    playPauseSwitch.innerHTML = `
+<svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+                      />
+                    </svg>
+`;
   }
+  console.log(playTrackOrder.paused);
 };
 
 // Arrow functions are not hoisted like function declarations,
